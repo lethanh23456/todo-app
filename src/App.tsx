b7d88e2b4d573fa8405f964a966e8c23f03aa422
lethanh23456 <thanhld2006.ptit@gmail.com>
2025-09-11 00:04:13 +0700

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import './App.css'
-// import hackathonGraphic from './assets/hackathon-graphic.svg'
-// import naverLogo from './assets/naver-logo.svg'
 
 function App() {
-  return (
-    <div className="container">
-      <div className="content">
-        {/* <img src={naverLogo} alt="NAVER Vietnam AI Hackathon" className="logo" /> */}
-        
-       <div className="text">todo</div>
-      </div>
-      
-      {/* <img className="graphic" src={hackathonGraphic} alt="" /> */}
-    </div>
-  )
+  return <div>App</div>;
 }
 
-export default App
+export default App;
