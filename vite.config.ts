a8aa75e4e613ef1c50413d122769add8358ebd12
lethanh23456 <thanhld2006.ptit@gmail.com>
2025-09-20 11:00:39 +0700

    set up and test lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(),tailwindcss(),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
